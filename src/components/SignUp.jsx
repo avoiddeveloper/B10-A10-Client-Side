4eb928e03d4d7f7b1e5596a85dd7ea7955be526d
avoiddeveloper <karimwdd03@gmail.com>
2025-03-05 17:34:16 +0600

    add authprovider and firebase initial setup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
 const SignUp = () => {
+
+    const handleForm = async (event) => {
+        event.preventDefault();
+        const form = new FormData(event.target);
+        const email = form.get("email");
+        const photo = form.get("photo");
+        const name = form.get("name");
+        const pass = form.get("pass");
+        console.log({ name, photo, email, pass })
+    };
+
     return (
         <section className=" w-11/12 min-h-screen flex flex-col justify-center">
             <div className="w-[50%] mx-auto bg-purple-200 rounded-xl space-y-5 p-5">
@@ -14,18 +22,18 @@ const SignUp = () => {
                 <div>
                     <div className="card w-full max-w-sm shrink-0 shadow-2xl mx-auto">
                         <div className="card-body text-center">
-                            <form className="fieldset">
+                            <form onSubmit={handleForm} className="fieldset">
                                 <label className="fieldset-label">Name</label>
-                                <input type="text" className="input w-full" placeholder="Enter Your Name" />
+                                <input type="text" name='name' className="input w-full" placeholder="Enter Your Name" />
 
                                 <label className="fieldset-label">Email</label>
-                                <input type="email" className="input w-full" placeholder="Enter Your Email" />
+                                <input type="email" name='email' className="input w-full" placeholder="Enter Your Email" />
 
                                 <label className="fieldset-label">Photo URL</label>
-                                <input type="text" className="input w-full" placeholder="Enter Your Photo URL" />
+                                <input type="text" name='photo' className="input w-full" placeholder="Enter Your Photo URL" />
 
                                 <label className="fieldset-label">Password</label>
-                                <input type="password" className="input w-full" placeholder="Password" />
+                                <input type="password" name='pass' className="input w-full" placeholder="Password" />
 
                                 <input type="submit" value="Sign Up" className="btn bg-purple-300 mt-4" />
                             </form>
@@ -35,6 +43,7 @@ const SignUp = () => {
                 </div>
             </div>
         </section>
+
     );
 };
 
